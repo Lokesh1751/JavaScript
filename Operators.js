116f57ab71e36d3cc1e14af1336d72a9e4a1bd6b
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-15 23:52:52 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,31 @@
+//Arthematic operators
+let a = 2;
+let b = 3;
+console.log(a + b);
+console.log(a % b);
+
+// Assignment operators
+let c = 43;
+c += 1;
+let d = 33;
+d /= 3;
+console.log(c);
+console.log(d);
+
+// const value cannot be incremented
+
+//Comparison Operators
+let e = 19;
+let f = 12;
+console.log(e == f); // return true of both are equal
+console.log(e != f); // return true of both are not equal
+console.log(e === f); // return true of both are equal and of same datatype
+console.log(e !== f); // return true of both are equal and of different datatype
+console.log(e >= f); // greater than equal to
+
+//Logical Operators
+let g = true;
+let h = false;
+console.log(g && h); // return true if both are true
+console.log(g || h); // return true if one of them is true
+console.log(!g); // return true of g is false
