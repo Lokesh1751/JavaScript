cdf716d2ee4c75ac8599fd145176dd8908ab0bb2
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-16 14:19:36 +0530

    Create Arrow-Functions.js

---
+++ Arrow-Functions.js
@@ -0,0 +1,10 @@
+//METHOD 1
+const greet = (a, b) => {
+  console.log("HEllo WOrld");
+  return a % b;
+};
+console.log(greet(12, 3));
+
+//METHOD 2
+const square = (a) => a * a;
+console.log(square(8));
